76d89a93ba0e95cf21028353a2c3c9927d9c0f6b
Maxx Lehmann <mglehman@mtu.edu>
2023-07-16 22:33:09 -0500

    throw 404 for post

---
+++ src/routes/blog/[slug]/+error.svelte
@@ -0,0 +1,2 @@
+<h1>404</h1>
+<h3>Post not found</h3>
--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,9 +1,15 @@
+import { error } from '@sveltejs/kit';
+
 export async function load({ params }) {
 	const post = await import(`../posts/${params.slug}.md`).catch((e: Error) => {
 		if (!e.message.startsWith('Unknown variable dynamic import')) {
 			throw e;
 		}
 	});
+	if (!post) {
+		throw error(404);
+	}
+
 	const content = post.default;
 
 	return {
